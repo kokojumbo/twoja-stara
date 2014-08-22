a004a9947966f302d9bb3be1e8628dab8620e8d2
dolatas <dodo.dolata@gmail.com>
2014-08-22 23:41:06 +0200

    android app init

---
+++ mobile/android/Epimelo/src/com/epimelo/app/Start.java
@@ -0,0 +1,25 @@
+
+package com.epimelo.app;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class Start
+    extends SherlockActivity
+{
+
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater();
+        return true;
+    }
+
+}
---
+++ mobile/android/Epimelo/target/generated-sources/r/com/epimelo/app/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.epimelo.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
